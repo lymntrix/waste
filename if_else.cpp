f6d820fc2a2e1ec675e5fd22b283ee3bf4063542
Rahul Chatterjee <44574873+lymntrix@users.noreply.github.com>
2020-04-20 19:50:56 +0530

    Add files via upload

---
+++ if_else.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int num;
+cout<<"enter a number";
+cin>>num;
+if(num>0)
+{
+cout<<" the number is positive :"<<num<<endl;
+}
+else
+{
+cout<<"you have entered a negetive number : "<<num<<endl;
+}
+return 0;
+}
+
